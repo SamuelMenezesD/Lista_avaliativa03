b338dc55b8c967a7d0f0d9c110e4802fef6fcd61
SamuelMenezesD <chicomenezes6505@gmail.com>
2024-03-24 18:52:30 -0300

    Exercicio 18

---
+++ Exercicio_18.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Exercicio_18 {
+    public static void executar(){
+
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Insira o sexo(M/H): ");
+        String sexo = leitor.nextLine();
+
+        System.out.println("Insira o nome: ");
+        String nome = leitor.nextLine();
+
+        System.out.println("Insira o salario: ");
+        double salariofixo = leitor.nextDouble();
+            
+        
+            
+        System.out.println("Insira idade: ");
+        int idade = leitor.nextInt();
+            
+        Funcionario18 funcionario18 = new Funcionario18(nome, salariofixo, sexo, idade);
+
+        System.out.println("" + funcionario18.escreverNome());
+        System.out.println("O salario com abono é: "+ funcionario18.receberSalario());
+
+        
+
+        leitor.close();
+
+    }
+    
+}
