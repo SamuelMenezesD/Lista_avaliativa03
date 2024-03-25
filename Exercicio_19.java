c04113df3663d3d62a58c98025a936bdbe0aff66
SamuelMenezesD <chicomenezes6505@gmail.com>
2024-03-25 19:53:33 -0300

    Exercicio 19

--- Exercicio_19.java
+++ Exercicio_19.java
@@ -5,7 +5,18 @@ public class Exercicio_19 {
     public static void executar(){
         Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Informe o lado 1 do triangulo: ");
+        int lado1 = leitor.nextInt();
 
+        System.out.println("Informe o lado 2 do triangulo: ");
+        int lado2 = leitor.nextInt();
+
+        System.out.println("Informe o lado 3 do triangulo: ");
+        int lado3 = leitor.nextInt();
+
+        Triangulo triangulo = new Triangulo(lado1, lado2, lado3);
+
+        System.out.println(triangulo.tipoTriangulo());
 
         leitor.close();
 
