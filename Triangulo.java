741b219c93ad4fcfd8024e132e1dbb8ff2b52667
SamuelMenezesD <chicomenezes6505@gmail.com>
2024-03-25 19:54:17 -0300

    Triangulo

--- Triangulo.java
+++ Triangulo.java
@@ -1,14 +1,36 @@
 public class Triangulo {
-    private int lado;
+    private int lado1;
+    private int lado2;
+    private int lado3;
 
-    public Triangulo(int lado){
-        this.lado = lado;
+    public Triangulo(int lado1, int lado2, int lado3){
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+
     }
 
     public String tipoTriangulo(){
 
-        if (condition) {
+        if (lado1 < lado2 + lado3) {
+            if (lado1==lado2&& lado2==lado3) {
+
+                return "Triangulo equilatero!";
+                
+            }
+            if (lado1==lado2|| lado2==lado3) {
+
+                return"Triangulo Isóscele!!";
+                
+            }
+            else{
+                return"Triangulo Escaleno!!!";
+            }
+
             
+        }
+        else{
+           return "Valor invalido!!!";
         }
 
     }
