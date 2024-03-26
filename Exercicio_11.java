0d8cb3c446648dc838b96e1afa423160d9f0b2df
SamuelMenezesD <chicomenezes6505@gmail.com>
2024-03-25 22:10:34 -0300

    Exercicio 11

--- Exercicio_11.java
+++ Exercicio_11.java
@@ -5,32 +5,9 @@ public class Exercicio_11 {
     public static void executar(){
         Scanner leitor = new Scanner(System.in);
 
-        int quanthomem = 0;
-        int quantmulher = 0;
+        PessoaGeneroContador pessoaGeneroContador = new PessoaGeneroContador();
 
-        for (int i = 0; i < 56; i++) {
-
-            System.out.println("Informe o nome: ");
-            String nome = leitor.nextLine();
-
-            System.out.println( nome +" é homem ou mulher? (H/M)");
-            String sexo = leitor.nextLine();
-            
-
-            if (sexo=="M"||sexo=="m") {
-                quantmulher++;
-                
-            }
-            if (sexo=="H"||sexo=="h") {
-                quanthomem++;
-            }
-            else{
-                System.out.println("Invalido");
-            }
-            
-        }
-        System.out.println("Total de homens: " + quanthomem);
-        System.out.println("Total de mulheres: " + quantmulher);
+        System.out.println(pessoaGeneroContador.contarSexo());
 
         leitor.close();
     }
