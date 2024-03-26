7453217a8e3b546d4ff168580905837a40de74c4
SamuelMenezesD <chicomenezes6505@gmail.com>
2024-03-25 21:01:06 -0300

    PessoaGeneroContador

--- PessoaGeneroContador.java
+++ PessoaGeneroContador.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 public class PessoaGeneroContador {
-    private String nome;
-    private String sexo;
+    
+    Scanner leitor = new Scanner(System.in);
+    public String contarSexo(){
+        int quanthomem = 0;
+        int quantmulher = 0;
+        for (int i = 0; i < 56; i++) {
 
-    public PessoaGeneroContador(String nome, String sexo) {
-        
-        this.nome = nome;
-        this.sexo = sexo;
-    }
-    public String getNome() {
-        return nome;
-    }
+            System.out.println("Informe o nome: ");
+            String nome = leitor.nextLine();
 
-    public String getSexo() {
-        return sexo;
+            System.out.println( nome +" é homem ou mulher? (H/M)");
+            String sexo = leitor.nextLine();
+            
+
+            if (sexo=="M"||sexo=="m") {
+                quantmulher++;
+                
+            }
+            if (sexo=="H"||sexo=="h") {
+                quanthomem++;
+            }
+            else{
+                return "Invalido";
+            }
+            
+        }
+        return "Total de homens: " + quanthomem + "Total de mulheres: " + quantmulher;
     }
 }
