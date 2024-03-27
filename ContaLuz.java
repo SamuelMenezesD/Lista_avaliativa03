d554eba1544d5630c85e07a41ff6c8a8911dcda5
SamuelMenezesD <chicomenezes6505@gmail.com>
2024-03-27 18:57:56 -0300

    Conta luz

---
+++ ContaLuz.java
@@ -0,0 +1,30 @@
+public class ContaLuz {
+    private int tipo;
+    private double consumo;
+
+    public ContaLuz(int tipo, double consumo){
+        this.consumo = consumo;
+        this.tipo = tipo;
+    }
+    public double calcularValorConta(int tipo, double consumo) {
+        double valor = 0;
+
+        switch (tipo) {
+            case 1:
+                valor = 0.60;
+                break;
+            case 2:
+                valor = 0.48;
+                break;
+            case 3:
+                valor = 1.29;
+                break;
+            default:
+                System.out.println("Tipo de cliente inválido!");
+                return 0;
+        }
+        double valorfinal = valor * consumo;
+        return valorfinal;
+    }
+    
+}
